5026efc15fabcf3f4eeb1881e4d16618f5af81c3
revanthuradi <revanthuradi9866@gmail.com>
2024-09-23 12:28:19 +0530

    api link updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import toast, { Toaster } from 'react-hot-toast';
-i
+
 const App = () => {
   
  
